39719c73a972fb54213e7d494c803c057e8220e8
mithun <mithunkiranparakkal@gmail.com>
2023-03-07 20:46:19 +0530

    first commit

---
+++ affnetz_v0/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Form from './components/RecaptchaForm';
+import {Paper} from "@mui/material"
+
+function App() {
+  return (
+    
+    <Form/> 
+    
+       
+     
+  );
+}
+
+export default App;
---
+++ affnetz_v0/src/components/RecaptchaForm.jsx
@@ -0,0 +1,76 @@
+import ReCAPTCHA from "react-google-recaptcha";
+import { useState } from "react";
+import {
+  Grid,
+  Paper,
+  Avatar,
+  TextField,
+  Button,
+ 
+} from "@mui/material";
+
+
+const Form = () => {
+  const [verified, setVerified] = useState(false);
+  const paperStyle = {
+    padding: 20,
+    height: "70vh",
+    width: 500,
+    margin: "20px auto",
+  };
+  const avatarStyle = { backgroundColor: "#1bbd7e" };
+  const btnstyle = { margin: "8px 0" };
+  const recaptchastyle = { marginTop:"10px" };
+  function onChange(value) {
+    console.log("Captcha value:", value);
+    setVerified(true);
+  }
+  return (
+    <Grid>
+      <Paper elevation={10} style={paperStyle}>
+        <Grid align="center">
+          <Avatar style={avatarStyle}></Avatar>
+          <h2>Sign up</h2>
+        </Grid>
+        <TextField
+          label="Firstname"
+          placeholder="Enter firstname"
+          variant="outlined"
+          fullWidth
+          required
+        />
+        <TextField
+          label="Lastname"
+          placeholder="Enter lastname"
+          variant="outlined"
+          fullWidth
+          required
+        />
+       
+        
+        <ReCAPTCHA
+          sitekey="6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI"
+          onChange={onChange}
+          data-theme="dark"
+          align="right"
+          style={recaptchastyle}
+          //size="invisible"
+          
+        />
+        <Button
+          type="submit"
+          color="primary"
+          variant="contained"
+          style={btnstyle}
+          fullWidth
+          disabled={!verified}
+        >
+          Sign up
+        </Button>
+        
+      </Paper>
+    </Grid>
+  );
+};
+
+export default Form;
